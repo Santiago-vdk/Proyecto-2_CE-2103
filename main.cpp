1f7e31e7f6b8e15b19c50c3ddd70e31b95fc684c
rafangel <rafangel1228@gmail.com>
2014-10-29 00:01:23 -0600

    cambios menores para pruebas

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "GUI.h"
 #include <QApplication>
+#include "string"
+#include "iostream"
+using namespace std;
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    GUI w;
-    w.show();
+    //GUI w;
+    //w.show();
+    string st="hola mundo nuevo";
+    //string *s=new string('"');
+    //st.append('"');
+//    string to = st.substr(1,st.find(" ",5)-1);
+//    cout<<"split"<<st.find(" ",5)<<to<<endl;
+
     return a.exec();
 }
