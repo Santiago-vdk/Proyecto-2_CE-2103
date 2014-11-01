6314994889e3d5a993a958f06aa70e5aa70c4ed2
rafangel <rafangel1228@gmail.com>
2014-11-01 12:04:44 -0600

    Cambios

--- dato.cpp
+++ dato.cpp
@@ -1,5 +1,28 @@
 #include "dato.h"
 
-Dato::Dato()
+Dato::Dato(string dato, string tipoDato)
 {
+    _dato = dato;
+    _tipoDato = tipoDato;
+}
+
+
+string Dato::getDato()
+{
+    return _dato;
+}
+
+string Dato::getTipoDato()
+{
+    return _tipoDato;
+}
+
+void Dato::setDato(string dato)
+{
+    _dato = dato;
+}
+
+void Dato::setTipoDato(string tipoDato)
+{
+    _tipoDato = tipoDato;
 }
--- dato.h
+++ dato.h
@@ -1,10 +1,22 @@
 #ifndef DATO_H
 #define DATO_H
+#include <string>
+using namespace std;
 
 class Dato
 {
 public:
-    Dato();
+    Dato(dato,tipoDato);
+    string getDato();
+    string getTipoDato();
+
+    void setDato(string dato);
+    void setTipoDato(string tipoDato);
+
+
+private:
+    string _dato;
+    string _tipoDato;
 };
 
 #endif // DATO_H
