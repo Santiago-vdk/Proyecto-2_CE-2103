2f056ea15c792025dba646b4aa40e3a470c22a07
Santiago-vdk <Santiago.vdk@gmail.com>
2014-11-22 02:13:23 -0600

    asdf

--- interprete.cpp
+++ interprete.cpp
@@ -2489,38 +2489,7 @@ bool Interprete::ejecutarUpdate(string sentencia)//se ejecuta update
                         }
                         if(tablaTmp->existeMetaDato(col)){
                             int j = tablaTmp->getMetaDato()->PosMetaDato(col);
-           if(tablaTmp->getMetaDato()->buscarPosicion(j)->getTipometaDato().compare("Integer")==0 ||
-                                    tablaTmp->getMetaDato()->buscarPosicion(j)->getTipometaDato().compare("Decimal")==0){
-                                std::string str = valor;
-                                QString test = QString::fromStdString(str);
-                                bool ok;
-                                bool ok2;
-                                test.toInt(&ok);
-                                test.toFloat(&ok2);
-                                if(ok || ok2){
-                                    tablaTmp->getMatrizDato()->setDato(i,j,valor);
-                                }
-                                else{
-                                    com Puerto;
-                                    Puerto.enviar("msg4");
-                                    return false;//error tipo
-                                }
-                            }
-                            else{
-                                tablaTmp->getMatrizDato()->setDato(i,j,valor);
-                            }
-                            if(tablaTmp->getMetaDato()->buscarPosicion(j)->getTipometaDato().compare("Integer") ||
-                                   ablaTmp->getMetaDato()->buscarPosicion(j)->getTipometaDato().compare("Decimal")){
-
-
-                                tablaTmp->getMatrizDato()->setDato(i,j,valor);
-                                else{
-
-                                }
-                            }
-                            else{
-                                ablaTmp->getMatrizDato()->setDato(i,j,valor);
-                            }
+                            tablaTmp->getMatrizDato()->setDato(i,j,valor);
 
                         }
                         if(!tablaTmp->existeMetaDato(col)){
