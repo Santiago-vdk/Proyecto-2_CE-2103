257d2939b7e36bb9ad30e7a4bf8d5b4070f8c91a
Santiago-vdk <Santiago.vdk@gmail.com>
2014-11-22 02:10:59 -0600

    Ajustes

--- main.cpp
+++ main.cpp
@@ -2,19 +2,14 @@
 #include <QApplication>
 #include "iostream"
 
-#include "compress.h"
+
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
- QApplication a(argc, argv);
+    QApplication a(argc, argv);
     GUI w;
-     w.show();
-
-   // compress archivo("/prueba.txt","/prueba.zip");
-    //archivo.encode();
-
-
+    w.show();
     return a.exec();
 }
